ab99ca3e082f741bbe9299328ba57db851fcb185
ikilllliki <rinka_16n1200079@nnn.ed.jp>
2017-07-03 14:37:29 +0900

    再帰なし版

--- recursive.js
+++ recursive.js
@@ -1,6 +1,18 @@
-function factorial(n) {
-    if (n != 0) { return n * factorial(n - 1); }
-    return 1;
-}
+// function factorial(n) {
+//     if (n != 0) { return n * factorial(n - 1); }
+//     return 1;
+// }
 
-console.log(factorial(5));
+// console.log(factorial(5));
+
+
+
+function factorial(n) {
+    let answer = 1;
+    for (let i = 0; i < n; i++) {
+        answer *= (n - 1);
+    }
+    return answer;
+ }
+
+ console.log (factorial (5));
